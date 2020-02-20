495a4cfb0dee69f179adc3e03ff651b334871ed6
1577665 <00888037163@cbmdf.gov.br>
2020-02-20 17:40:12 -0300

    Campo Dados Militares com + e -

---
+++ static/js/diarias.js
@@ -0,0 +1,38 @@
+
+//Início dos Parâmetros iniciais
+
+		$(".data").datepicker({
+		    changeMonth: true,
+            changeYear: true,
+            dateFormat: 'dd/mm/yy',
+            dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+            dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+            dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+            monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+            monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+            nextText: 'Próximo',
+            prevText: 'Anterior',
+
+        });
+        $(".data").mask("00/00/0000");
+        $(".cpf").mask("000.000.000-00");
+//Fim dos Parâmetros Iniciais
+
+//Início das Linhas de Militares
+
+        $("div.militares").on('click','input.botao-add', function(e) {
+                n+=1;
+//                TODO: Não está somando o 1. Verificar
+                var elem = document.getElementById("militares")
+                elem.insertAdjacentHTML('beforeend', militar(n));
+            }
+        );
+        $("div.militares").on('click','input.botao-del', function(e) {
+                if (n != 1) {
+                    document.getElementById("mil-"+this.value).remove();
+                    n-=1;
+                    }
+            }
+        );
+
+//Fim das Linhas de Militares
---
+++ static/js/variaveis.js
@@ -0,0 +1,66 @@
+//var militar_head = `<div class="form-row mil-`
+function militar(n) {
+return (`<div class="form-row militares-row mil-${n}" id="mil-${n}">
+				<div class="form-group col-md-1">
+				  <label>Posto/Grad</label>
+				  <select name="posto" class="form-control" required id="posto-${n}">
+					 <option value="null"></option>
+					  <!--TODO: Adicionar Cmt Geral. Aqui e no JSON-->
+					 <option value="CORONEL">CEL</option>
+					 <option value="TENENTE-CORONEL">TC</option>
+					 <option value="MAJOR">MAJ</option>
+					 <option value="CAPITAO">CAP</option>
+					 <option value="PRIMEIRO TENENTE">1º TEN</option>
+					 <option value="SEGUNDO TENENTE">2º TEN</option>
+					 <option value="ASPIRANTE">ASP</option>
+					 <option value="SUBTENENTE">ST</option>
+					 <option value="PRIMEIRO SARGENTO">1º SGT</option>
+					 <option value="SEGUNDO SARGENTO">2º SGT</option>
+					 <option value="TERCEIRO SARGENTO">3º SGT</option>
+					 <option value="CABO">CB</option>
+					 <option value="SOLDADO PRIMEIRA CLASSE">SD/1</option>
+					 <option value="SOLDADO SEGUNDA CLASSE">SD/2</option>
+					 <option value="CADETE DEMAIS ANOS">CAD/1</option>
+					 <option value="CADETE ULTIMO ANO">CAD/2</option>
+				  </select>
+				</div>
+				<div class="form-group col-md-1">
+				  <label>Matrícula</label>
+				  <input type="text" id="matricula-${n}" class="form-control">
+				</div>
+				<div class="form-group col-md-2">
+				  <label>Nome</label>
+				  <input type="text" id="nome-${n}" class="form-control">
+				</div>
+				<div class="form-group col-md-2">
+				  <label>CPF</label>
+				  <input type="text" id="cpf-${n}" class="form-control cpf">
+				</div>
+				<div class="form-group col-md-1">
+				  <label>Banco</label>
+		<!--			TODO: Usar https://raw.githubusercontent.com/guibranco/BancosBrasileiros/master/bancos.json como fonte de pesquisa-->
+				  <input type="text" id="banco-${n}" class="form-control">
+				</div>
+				<div class="form-group col-md-1">
+				  <label>Agência</label>
+				  <input type="text" id="agencia-${n}" class="form-control">
+				</div>
+				<div class="form-group col-md-1">
+				  <label>Conta</label>
+				  <input type="text" id="conta-${n}" class="form-control">
+				</div>
+				<div class="form-group col-md-2">
+			  		<label>Viajará com Dependente?</label>
+			  		<select name="bool_dep" class="form-control" required id="missao-${n}">
+					<option value="True">SIM</option>
+					<option value="False">NÃO</option>
+<!--			TODO: Aparecer apenas se pagar Ajuda de Custo-->
+			  		</select>
+				</div>
+				<div>
+					<input type="submit" class="botao-add" id="add-${n}" name="form.button.add" value="${n}" title="Adicionar linha">
+					<input type="submit" class="botao-del" id="del-${n}" name="form.button.del" value="${n}" title="Excluir linha">
+				</div>
+			</div>
+			`);
+			}
